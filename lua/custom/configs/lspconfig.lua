8cfa26a0e6c2181a056f540d7dd3d0d8c3f65d3e
jameschee <litcoderr@gmail.com>
2023-06-26 00:42:22 +0900

    init

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,25 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git")
+}
+
+lspconfig.dartls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "dart", 'language-server', '--protocol=lsp' },
+}
+
